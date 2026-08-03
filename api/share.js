e15f306226f3894ae2f3d14650210c51dc6bfb90
Brian Crouse <briancrouse77@gmail.com>
2026-08-03 12:47:44 -0500

    Add og:url and og:site_name meta tags to api/share.js to satisfy Facebook debugger validation

--- api/share.js
+++ api/share.js
@@ -115,6 +115,8 @@ module.exports = async (req, res) => {
   <meta property="og:image:width" content="800">
   <meta property="og:image:height" content="800">
   <meta property="og:type" content="website">
+  <meta property="og:url" content="https://www.spirittreeceramics.com/pot/${id}">
+  <meta property="og:site_name" content="Spirit Tree Ceramics">
   
   <meta name="twitter:card" content="summary_large_image">
   <meta name="twitter:title" content="${escapeHtml(title)}">
